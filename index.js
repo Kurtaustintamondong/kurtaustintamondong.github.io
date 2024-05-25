a20ae1f72d26af96b0cafff3f1f4455527e23780
Kurtaustintamondong <renielcastro00@gmail.com>
2024-05-25 19:10:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const btnLike1 = document.getElememtById("btnLike1")
-const countLike1 = document.getElemetById("countLike1")
+const btnLike1 = document.getElementById("btnLike1")
+const countLike1 = document.getElementById("countLike1")
 function clickLike1(){
   let totalLikes = parseInt(countLike1.value) + 1
   countLike1.textContent = totalLikes.toString()
