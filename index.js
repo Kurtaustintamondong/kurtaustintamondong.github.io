58442b31b5c62f68f34ae62cb53ca0cbf2656817
Kurtaustintamondong <renielcastro00@gmail.com>
2024-05-25 18:13:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const countLike1 = document.getElemetById("countLike1")
 
 function clickLike1(){
   let totalLikes = parseInt(countLike1.value) + 1
-  countLike1.
+  countLike1.textContent = totalLikes.toString()
 }  
