67c5f37cca73e80e97a68608a180ee9a97d4eb48
Kurtaustintamondong <renielcastro00@gmail.com>
2024-06-08 18:17:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const submit = document.getElementById("submit")
 const comment = document.getElementById("comment")
 const commentbox = document.getElementById("commentbox")
 function submitComment(){
+  
 commentbox.textContent += comment.value.toString() + "\n"
+  comment.value=""
 }
 submit.addEventListener("click", submitComment)
